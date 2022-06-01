ae7de2d648150bca9038acf65b80eb0d6d83ec4f
ritamf <ritaferrolho@ua.pt>
2022-06-01 11:48:38 +0100

    update openMP tests, for chessBig.pgm

--- cp_harrisDetector/testsOpenMP/test_generateOpenMPfiles.py
+++ cp_harrisDetector/testsOpenMP/test_generateOpenMPfiles.py
@@ -8,20 +8,20 @@ with open("testTemplate_harrisDetectorOpenMP.c") as f:
 
 # list of tests done in the first loop and second loop
 tests = [
-    ("schedule(guided,8)", "schedule(guided,8)"),
-    ("schedule(guided,8)", "schedule(guided,2)"),
-    ("schedule(guided,8)", "schedule(guided,30)"),
-    ("schedule(static,8)", "schedule(static,8)"),
-    ("schedule(dynamic,8)", "schedule(dynamic,8)"),
+    "schedule(guided,8)",
+    "schedule(guided,2)",
+    "schedule(guided,30)",
+    "schedule(static,8)",
+    "schedule(dynamic,8)",
 
-    ("schedule(guided,8) collapse(2)", "schedule(guided,8) collapse(2)"),
-    ("schedule(guided,8) collapse(2)", "schedule(guided,2) collapse(2)"),
-    ("schedule(guided,8) collapse(2)", "schedule(guided,30) collapse(2)"),
-    ("schedule(static,8) collapse(2)", "schedule(static,8) collapse(2)"),
-    ("schedule(dynamic,8) collapse(2)", "schedule(dynamic,8) collapse(2)"),
+    "schedule(guided,8) collapse(2)",
+    "schedule(guided,2) collapse(2)",
+    "schedule(guided,30) collapse(2)",
+    "schedule(static,8) collapse(2)",
+    "schedule(dynamic,8) collapse(2)"
 ]
 
-test_scripts = [f"test_harrisDetectorOpenMP_{i}.c" for i in range(len(tests))]
+test_scripts = [f"test_harrisDetectorOpenMP_{tests[i].replace('(','').replace(')','').replace(',','').replace(' ','')}.c" for i in range(len(tests))]
 test_results = ""
 
 # execute all tests
@@ -29,13 +29,13 @@ for i in range(len(tests)):
 
     # format template code, save it in a script 
     with open(test_scripts[i],"w") as f:
-        f.write(lines.format(tests[i][0],tests[i][1]))
+        f.write(lines.format(tests[i],tests[i]))
 
     # compile script
     os.system(f"g++ -fopenmp -O3 -ICommon {test_scripts[i]} -o {test_scripts[i][:-2]}")
 
     # execute code and print results
-    print(f"{tests[i][0]}, {tests[i][1]}")
+    print(tests[i])
     os.system(f"./{test_scripts[i][:-2]}")
     print("\nwaiting 10 secs...")
 
