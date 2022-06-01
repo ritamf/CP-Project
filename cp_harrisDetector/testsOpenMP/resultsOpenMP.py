821f647f54c43fb858631175e8fa72748b1e2809
ritamf <ritaferrolho@ua.pt>
2022-06-01 14:55:28 +0100

    openMP: get host and openMP processing time of each test, calculate efficiency of each test, present results in latex table

---
+++ cp_harrisDetector/testsOpenMP/resultsOpenMP.py
@@ -0,0 +1,22 @@
+with open("rawResultsOpenMP.txt") as f:
+    results = f.readlines()
+
+resDict = dict()
+
+for i, string in enumerate(results):
+    if string.startswith("schedule"):
+        valueHost = float(results[i+1].split("Host processing time: ")[1].split(" (ms)")[0])
+        valueOpenMP = float(results[i+2].split("OpenMP processing time: ")[1].split(" (ms)")[0])
+        efficiency = (valueHost - valueOpenMP) / valueHost * 100
+        resDict[string[:-1]] = {"valueHost":valueHost, "valueOpenMP":valueOpenMP, "efficiency" : f"{efficiency:.{2}f}\%"}
+
+with open("statsTableTemplate.sty") as f:
+    table_str = "".join(f.readlines()) + "\n"
+
+for k, v in resDict.items():
+    table_str+= "\multicolumn{{1}}{{|l|}}{{ {} }} & \multicolumn{{1}}{{l|}}{{ {} }} & \multicolumn{{1}}{{l|}}{{ {} }} & \multicolumn{{1}}{{l|}}{{ {} }} &  \\\ \cline{{1-4}} \n".format(k, v["valueHost"], v["valueOpenMP"], v["efficiency"]) # usou-se uma "\" a mais e "\n" no fim
+
+table_str+= "\n \end{tabular} \n \end{table} \n"
+
+with open("statsTableResults.sty","w") as f:
+    f.write(table_str)
